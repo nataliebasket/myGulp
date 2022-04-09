94121477bc208570be8451fafe1b193d2bad00c3
nataliebasket <89080528+nataliebasket@users.noreply.github.com>
2022-04-09 21:31:15 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,72 @@
-import gulp from "gulp"
+import gulp from "gulp";
+import htmlmin from "gulp-htmlmin";
+import browserSync from "browser-sync";
+import plumber from "gulp-plumber";
+import del from "del";
+import sass from "gulp-dart-sass";
+import postcss from "gulp-postcss";
+import importScss from "postcss-import";
+import postScss from "postcss-scss";
 
-const {src, dest, series} = gulp;
+const {src, dest, watch, series, parallel} = gulp;
 
 export function html()  {
-    console.log("Hello!!");
+    return src("./source/*.html")
+    .pipe(plumber())
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(dest("./public"))
-   
 }
 
-// export function clear () {
-//     return del("./public");
-//   }
+export function copyImg () {
+    return src("./source/img/**/*.{png,jpg}")
+    .pipe(dest("public/img"))
+  }
+
+
+export function styles () {
+  return src("./source/scss/*.scss", { sourcemaps: true})
+    .pipe(plumber())
+    .pipe(postcss([importScss()], { syntax: postScss }))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("./public/styles", { sourcemaps: true}))
+    .pipe(browserSync.stream());
+}
+
+//Сервер
+export function server (done) {
+    browserSync.init({
+      server: {
+          baseDir: "./public"
+      },
+      notify: false,
+      ui: false
+    });
+    done();
+  }
+
+function reloadServer (done) {
+browserSync.reload();
+done();
+}
+
+function watcher () {
+    watch("./source/scss/blocks/*.scss", series(styles));
+    watch("./source/**/*.html", series(html, reloadServer));
+  }
+
+export function clear () {
+    return del("./public");
+  }
 
 export default series (
-// clear,
-// parallel(
-    // styles,
+  clear,
+  parallel(
+    styles,
     html,
-    // copyImg,
-    // copyOther
-// ),
-// series (
-    // server,
-    // watcher
-// )
+    copyImg,
+  ),
+  series (
+    server,
+    watcher
+    )
 );
